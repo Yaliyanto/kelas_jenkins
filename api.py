15a38866ea3982aa687faba189bd9263200a767f
Yali Yanto Silitonga <yali.silitonga@jatis.com>
2025-11-02 17:19:57 +0700

    namabhin

--- api.py
+++ api.py
@@ -7,8 +7,8 @@ headers = {"x-api-key": "reqres-free-v1"}
 response = requests.get(url, headers=headers)
 
 print("Status Code:", response.status_code)
-print("Response Body:")
+# print("Response Body:")
 print(json.dumps(response.json(), indent=4))  # <-- rapihin JSON
 
 assert response.status_code == 200, f"Expected 200, got {response.status_code}"
-print("✅ Test passed! Response status is 200.")
+print(" Test passed! Response status is 200.")
